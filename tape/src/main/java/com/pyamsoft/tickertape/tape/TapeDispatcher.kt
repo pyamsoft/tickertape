ff9cdec157b2d8342ffac4a470e54061de25209f
pyamsoft <developer.pyamsoft@gmail.com>
2021-12-27 16:30:11 -0800

    delete bad group name

--- tape/src/main/java/com/pyamsoft/tickertape/tape/TapeDispatcher.kt
+++ tape/src/main/java/com/pyamsoft/tickertape/tape/TapeDispatcher.kt
@@ -75,6 +75,7 @@ internal constructor(
 
       Timber.d("Create notification channel and group: ${channelInfo.id} ${channelInfo.title}")
       channelCreator.apply {
+        deleteNotificationChannelGroup(channelInfo.id)
         createNotificationChannelGroup(notificationGroup)
         createNotificationChannel(notificationChannel)
       }

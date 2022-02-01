672528b0491b3e923285f2932e7aa465a80bd090
pyamsoft <developer.pyamsoft@gmail.com>
2022-02-01 00:43:42 -0800

    Remove bottom padding

--- portfolio/src/main/java/com/pyamsoft/tickertape/portfolio/dig/position/add/PositionAddScreen.kt
+++ portfolio/src/main/java/com/pyamsoft/tickertape/portfolio/dig/position/add/PositionAddScreen.kt
@@ -1,20 +1,10 @@
 package com.pyamsoft.tickertape.portfolio.dig.position.add
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -84,7 +74,9 @@ fun PositionAddScreen(
         ) {
           NumberOfShares(
               modifier =
-                  Modifier.fillMaxWidth().padding(bottom = MaterialTheme.keylines.baseline).focusRequester(focusRequester),
+                  Modifier.fillMaxWidth()
+                      .padding(bottom = MaterialTheme.keylines.baseline)
+                      .focusRequester(focusRequester),
               isOption = isOption,
               readOnly = isReadOnly,
               numberOfShares = numberOfShares,
@@ -109,7 +101,6 @@ fun PositionAddScreen(
           )
 
           SubmitSection(
-              modifier = Modifier.padding(bottom = MaterialTheme.keylines.content),
               isEnabled = isSubmitEnabled,
               onSubmit = {
                 onSubmit()
@@ -169,7 +160,8 @@ private fun DateOfPurchase(
       verticalAlignment = Alignment.CenterVertically,
   ) {
     Icon(
-        modifier = Modifier.padding(end = ImageDefaults.IconSize).alpha(TextFieldDefaults.IconOpacity),
+        modifier =
+            Modifier.padding(end = ImageDefaults.IconSize).alpha(TextFieldDefaults.IconOpacity),
         imageVector = Icons.Filled.Today,
         contentDescription = "Date of Purchase",
     )

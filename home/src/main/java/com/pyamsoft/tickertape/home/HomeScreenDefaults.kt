d4d5d2bf1a5eb6cb3612753df29c703447354ecb
pyamsoft <developer.pyamsoft@gmail.com>
2022-04-04 20:28:44 -0700

    bump size

--- home/src/main/java/com/pyamsoft/tickertape/home/HomeScreenDefaults.kt
+++ home/src/main/java/com/pyamsoft/tickertape/home/HomeScreenDefaults.kt
@@ -6,5 +6,5 @@ internal object HomeScreenDefaults {
   val ITEM_WIDTH_DP = 350.dp
   val CHART_HEIGHT_DP = 160.dp
   val WATCHLIST_HEIGHT_DP = 108.dp
-  val PORTFOLIO_HEIGHT_DP = 128.dp
+  val PORTFOLIO_HEIGHT_DP = 140.dp
 }

9c481dc9626544a0136db4cdee32ccdf1f9c11b5
pyamsoft <developer.pyamsoft@gmail.com>
2022-07-14 15:15:59 -0700

    Base for portfolio screen

--- portfolio/src/main/java/com/pyamsoft/tickertape/portfolio/PortfolioScreen.kt
+++ portfolio/src/main/java/com/pyamsoft/tickertape/portfolio/PortfolioScreen.kt
@@ -1,33 +1,20 @@
 package com.pyamsoft.tickertape.portfolio
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import coil.ImageLoader
-import com.google.accompanist.insets.navigationBarsHeight
-import com.google.accompanist.insets.navigationBarsPadding
 import com.google.accompanist.insets.statusBarsHeight
-import com.google.accompanist.swiperefresh.SwipeRefresh
-import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.pyamsoft.pydroid.theme.keylines
 import com.pyamsoft.tickertape.portfolio.item.PorfolioSummaryItem
 import com.pyamsoft.tickertape.portfolio.item.PortfolioItem
-import com.pyamsoft.tickertape.quote.SearchBar
-import com.pyamsoft.tickertape.quote.add.NewTickerFab
+import com.pyamsoft.tickertape.quote.BaseListScreen
 import com.pyamsoft.tickertape.stocks.api.EquityType
-import com.pyamsoft.tickertape.ui.ErrorScreen
-import com.pyamsoft.tickertape.ui.FabDefaults
-import com.pyamsoft.tickertape.ui.PolinaGolubevaScreen
 import com.pyamsoft.tickertape.ui.test.createNewTestImageLoader
 import kotlinx.coroutines.CoroutineScope
 
@@ -46,263 +33,64 @@ fun PortfolioScreen(
     onFabClick: () -> Unit,
     onRegenerateList: CoroutineScope.() -> Unit,
 ) {
-  val loading = state.isLoading
-  val scaffoldState = rememberScaffoldState()
-
-  Scaffold(
-      modifier = modifier,
-      scaffoldState = scaffoldState,
-  ) {
-    SwipeRefresh(
-        state = rememberSwipeRefreshState(isRefreshing = loading),
-        onRefresh = onRefresh,
-    ) {
-      Content(
-          modifier = Modifier.fillMaxSize(),
-          state = state,
-          imageLoader = imageLoader,
-          navBarBottomHeight = navBarBottomHeight,
-          onRefresh = onRefresh,
-          onSelect = onSelect,
-          onDelete = onDelete,
-          onSearchChanged = onSearchChanged,
-          onTabUpdated = onTabUpdated,
-          onFabClick = onFabClick,
-          onRegenerateList = onRegenerateList,
-      )
-    }
-  }
-}
-
-@Composable
-private fun Content(
-    modifier: Modifier = Modifier,
-    state: PortfolioViewState,
-    imageLoader: ImageLoader,
-    navBarBottomHeight: Int,
-    onSelect: (PortfolioStock) -> Unit,
-    onDelete: (PortfolioStock) -> Unit,
-    onSearchChanged: (String) -> Unit,
-    onRefresh: () -> Unit,
-    onTabUpdated: (EquityType) -> Unit,
-    onFabClick: () -> Unit,
-    onRegenerateList: CoroutineScope.() -> Unit,
-) {
   val isLoading = state.isLoading
-
-  val density = LocalDensity.current
-  val bottomPaddingDp =
-      remember(
-          density,
-          navBarBottomHeight,
-      ) { density.run { navBarBottomHeight.toDp() } }
-  val contentPadding = MaterialTheme.keylines.content
-  val fabBottomPadding =
-      remember(bottomPaddingDp, contentPadding) { bottomPaddingDp + contentPadding }
-
-  Box(
-      modifier = modifier,
-      contentAlignment = Alignment.BottomCenter,
-  ) {
-    Portfolio(
-        modifier = Modifier.fillMaxSize(),
-        state = state,
-        imageLoader = imageLoader,
-        navBarBottomHeight = bottomPaddingDp,
-        onSelect = onSelect,
-        onDelete = onDelete,
-        onSearchChanged = onSearchChanged,
-        onTabUpdated = onTabUpdated,
-        onRefresh = onRefresh,
-        onRegenerateList = onRegenerateList,
-    )
-
-    NewTickerFab(
-        visible = !isLoading,
-        modifier =
-            Modifier.padding(MaterialTheme.keylines.content)
-                .navigationBarsPadding(bottom = true)
-                .padding(bottom = fabBottomPadding),
-        onClick = onFabClick,
-    )
-  }
-}
-
-@Composable
-@OptIn(ExperimentalFoundationApi::class)
-private fun Portfolio(
-    modifier: Modifier = Modifier,
-    state: PortfolioViewState,
-    imageLoader: ImageLoader,
-    navBarBottomHeight: Dp,
-    onSelect: (PortfolioStock) -> Unit,
-    onDelete: (PortfolioStock) -> Unit,
-    onSearchChanged: (String) -> Unit,
-    onTabUpdated: (EquityType) -> Unit,
-    onRefresh: () -> Unit,
-    onRegenerateList: CoroutineScope.() -> Unit,
-) {
-  val error = state.error
-  val portfolio = state.portfolio
+  val pageError = state.error
   val stocks = state.stocks
   val search = state.query
   val tab = state.section
 
-  val isEmptyList = remember(stocks) { stocks.isEmpty() }
-
-  LazyColumn(
+  BaseListScreen(
       modifier = modifier,
-      contentPadding = PaddingValues(horizontal = MaterialTheme.keylines.baseline),
+      navBarBottomHeight = navBarBottomHeight,
+      imageLoader = imageLoader,
+      isLoading = isLoading,
+      pageError = pageError,
+      list = stocks,
+      search = search,
+      tab = tab,
+      emptyStateImage = R.drawable.portfolio_empty,
+      emptyStateLabel = "Nothing in your portfolio, add a position!",
+      onRefresh = onRefresh,
+      onSearchChanged = onSearchChanged,
+      onTabUpdated = onTabUpdated,
+      onFabClick = onFabClick,
+      onRegenerateList = onRegenerateList,
+      itemKey = { index, stock -> "${stock.holding.symbol().symbol()}-${index}" },
+      renderHeader = {
+        PortfolioSummary(
+            modifier = Modifier.fillMaxWidth(),
+            state = state,
+        )
+      },
+      renderListItem = { stock ->
+        PortfolioItem(
+            modifier = Modifier.fillMaxWidth(),
+            stock = stock,
+            onSelect = onSelect,
+            onDelete = onDelete,
+        )
+      },
+  )
+}
+
+@Composable
+private fun PortfolioSummary(
+    modifier: Modifier = Modifier,
+    state: PortfolioViewState,
+) {
+  val portfolio = state.portfolio
+
+  Column(
+      modifier = modifier,
   ) {
-    if (!portfolio.isEmpty) {
-      item {
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-        ) {
-          Spacer(
-              modifier = Modifier.statusBarsHeight(),
-          )
-          PorfolioSummaryItem(
-              modifier =
-                  Modifier.fillMaxWidth().padding(horizontal = MaterialTheme.keylines.baseline),
-              portfolio = portfolio,
-          )
-        }
-      }
-    }
-
-    stickyHeader {
-      Column(
-          modifier = Modifier.fillMaxWidth(),
-      ) {
-        Spacer(
-            modifier = Modifier.statusBarsHeight(),
-        )
-        SearchBar(
-            modifier = Modifier.fillMaxWidth(),
-            search = search,
-            currentTab = tab,
-            onSearchChanged = onSearchChanged,
-            onTabUpdated = onTabUpdated,
-            onRegenerateList = onRegenerateList,
-        )
-      }
-    }
-
-    when {
-      error != null -> {
-        item {
-          ErrorState(
-              modifier = Modifier.fillMaxSize(),
-              imageLoader = imageLoader,
-              error = error,
-              onRefresh = onRefresh,
-          )
-        }
-      }
-      isEmptyList -> {
-        item {
-          EmptyState(
-              modifier = Modifier.fillMaxWidth(),
-              imageLoader = imageLoader,
-          )
-        }
-      }
-      else -> {
-        itemsIndexed(
-            items = stocks,
-            key = { index, item -> "${item.holding.symbol().symbol()}-${index}" },
-        ) { index, ps ->
-          if (index == 0) {
-            Spacer(
-                modifier = Modifier.height(MaterialTheme.keylines.content),
-            )
-          }
-
-          PortfolioItem(
-              modifier = Modifier.fillMaxWidth(),
-              stock = ps,
-              onSelect = onSelect,
-              onDelete = onDelete,
-          )
-
-          Spacer(
-              modifier = Modifier.height(MaterialTheme.keylines.content),
-          )
-        }
-      }
-    }
-
-    item {
-      Spacer(
-          modifier =
-              Modifier.navigationBarsHeight(
-                  additional =
-                      navBarBottomHeight +
-                          FabDefaults.FAB_OFFSET_DP +
-                          (MaterialTheme.keylines.content * 2),
-              ),
-      )
-    }
+    Spacer(
+        modifier = Modifier.statusBarsHeight(),
+    )
+    PorfolioSummaryItem(
+        modifier = Modifier.fillMaxWidth().padding(horizontal = MaterialTheme.keylines.baseline),
+        portfolio = portfolio,
+    )
   }
-}
-
-@Composable
-private fun EmptyState(
-    modifier: Modifier = Modifier,
-    imageLoader: ImageLoader,
-) {
-  PolinaGolubevaScreen(
-      modifier = modifier,
-      imageLoader = imageLoader,
-      image = R.drawable.portfolio_empty,
-      bottomContent = {
-        Text(
-            modifier = Modifier.padding(horizontal = MaterialTheme.keylines.content),
-            text = "Nothing in your portfolio, add a position!",
-            style = MaterialTheme.typography.h6,
-        )
-      },
-  )
-}
-
-@Composable
-private fun ErrorState(
-    modifier: Modifier = Modifier,
-    imageLoader: ImageLoader,
-    error: Throwable,
-    onRefresh: () -> Unit,
-) {
-  ErrorScreen(
-      modifier = modifier,
-      imageLoader = imageLoader,
-      bottomContent = {
-        Text(
-            textAlign = TextAlign.Center,
-            text = error.message ?: "An unexpected error occurred",
-            style =
-                MaterialTheme.typography.body1.copy(
-                    color = MaterialTheme.colors.error,
-                ),
-        )
-
-        Text(
-            modifier = Modifier.padding(top = MaterialTheme.keylines.content),
-            textAlign = TextAlign.Center,
-            text = "Please try again later.",
-            style = MaterialTheme.typography.body2,
-        )
-
-        Button(
-            modifier = Modifier.padding(top = MaterialTheme.keylines.content),
-            onClick = onRefresh,
-        ) {
-          Text(
-              text = "Refresh",
-          )
-        }
-      },
-  )
 }
 
 @Preview
---
+++ quote/src/main/java/com/pyamsoft/tickertape/quote/BaseListScreen.kt
@@ -0,0 +1,349 @@
+package com.pyamsoft.tickertape.quote
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import coil.ImageLoader
+import com.google.accompanist.insets.navigationBarsHeight
+import com.google.accompanist.insets.navigationBarsPadding
+import com.google.accompanist.insets.statusBarsHeight
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.pyamsoft.pydroid.theme.keylines
+import com.pyamsoft.tickertape.quote.add.NewTickerFab
+import com.pyamsoft.tickertape.stocks.api.EquityType
+import com.pyamsoft.tickertape.ui.ErrorScreen
+import com.pyamsoft.tickertape.ui.FabDefaults
+import com.pyamsoft.tickertape.ui.PolinaGolubevaScreen
+import com.pyamsoft.tickertape.ui.test.createNewTestImageLoader
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+@JvmOverloads
+fun <T : Any> BaseListScreen(
+    modifier: Modifier = Modifier,
+    imageLoader: ImageLoader,
+    isLoading: Boolean,
+    pageError: Throwable?,
+    list: List<T>,
+    search: String,
+    tab: EquityType,
+    navBarBottomHeight: Int = 0,
+    @DrawableRes emptyStateImage: Int,
+    emptyStateLabel: String,
+    onRefresh: () -> Unit,
+    onSearchChanged: (String) -> Unit,
+    onTabUpdated: (EquityType) -> Unit,
+    onFabClick: () -> Unit,
+    onRegenerateList: CoroutineScope.() -> Unit,
+    itemKey: (Int, T) -> String,
+    renderHeader: (@Composable () -> Unit)? = null,
+    renderListItem: @Composable (T) -> Unit,
+) {
+  val scaffoldState = rememberScaffoldState()
+
+  Scaffold(
+      modifier = modifier,
+      scaffoldState = scaffoldState,
+  ) {
+    SwipeRefresh(
+        state = rememberSwipeRefreshState(isRefreshing = isLoading),
+        onRefresh = onRefresh,
+    ) {
+      Content(
+          modifier = Modifier.fillMaxSize(),
+          imageLoader = imageLoader,
+          isLoading = isLoading,
+          navBarBottomHeight = navBarBottomHeight,
+          onRefresh = onRefresh,
+          onSearchChanged = onSearchChanged,
+          onTabUpdated = onTabUpdated,
+          onFabClick = onFabClick,
+          onRegenerateList = onRegenerateList,
+          emptyStateImage = emptyStateImage,
+          emptyStateLabel = emptyStateLabel,
+          itemKey = itemKey,
+          renderHeader = renderHeader,
+          renderListItem = renderListItem,
+          pageError = pageError,
+          list = list,
+          search = search,
+          tab = tab,
+      )
+    }
+  }
+}
+
+@Composable
+private fun <T : Any> Content(
+    modifier: Modifier = Modifier,
+    imageLoader: ImageLoader,
+    pageError: Throwable?,
+    list: List<T>,
+    search: String,
+    tab: EquityType,
+    isLoading: Boolean,
+    navBarBottomHeight: Int,
+    @DrawableRes emptyStateImage: Int,
+    emptyStateLabel: String,
+    onSearchChanged: (String) -> Unit,
+    onRefresh: () -> Unit,
+    onTabUpdated: (EquityType) -> Unit,
+    onFabClick: () -> Unit,
+    onRegenerateList: CoroutineScope.() -> Unit,
+    itemKey: (Int, T) -> String,
+    renderHeader: (@Composable () -> Unit)? = null,
+    renderListItem: @Composable (T) -> Unit,
+) {
+  val density = LocalDensity.current
+  val bottomPaddingDp =
+      remember(
+          density,
+          navBarBottomHeight,
+      ) { density.run { navBarBottomHeight.toDp() } }
+  val contentPadding = MaterialTheme.keylines.content
+  val fabBottomPadding =
+      remember(bottomPaddingDp, contentPadding) { bottomPaddingDp + contentPadding }
+
+  Box(
+      modifier = modifier,
+      contentAlignment = Alignment.BottomCenter,
+  ) {
+    ListSection(
+        modifier = Modifier.fillMaxSize(),
+        imageLoader = imageLoader,
+        navBarBottomHeight = bottomPaddingDp,
+        emptyStateImage = emptyStateImage,
+        emptyStateLabel = emptyStateLabel,
+        onSearchChanged = onSearchChanged,
+        onTabUpdated = onTabUpdated,
+        onRefresh = onRefresh,
+        onRegenerateList = onRegenerateList,
+        itemKey = itemKey,
+        renderHeader = renderHeader,
+        renderListItem = renderListItem,
+        pageError = pageError,
+        list = list,
+        search = search,
+        tab = tab,
+    )
+
+    NewTickerFab(
+        visible = !isLoading,
+        modifier =
+            Modifier.padding(MaterialTheme.keylines.content)
+                .navigationBarsPadding(bottom = true)
+                .padding(bottom = fabBottomPadding),
+        onClick = onFabClick,
+    )
+  }
+}
+
+@Composable
+@OptIn(ExperimentalFoundationApi::class)
+private fun <T : Any> ListSection(
+    modifier: Modifier = Modifier,
+    imageLoader: ImageLoader,
+    pageError: Throwable?,
+    list: List<T>,
+    search: String,
+    tab: EquityType,
+    navBarBottomHeight: Dp,
+    @DrawableRes emptyStateImage: Int,
+    emptyStateLabel: String,
+    onSearchChanged: (String) -> Unit,
+    onTabUpdated: (EquityType) -> Unit,
+    onRefresh: () -> Unit,
+    onRegenerateList: CoroutineScope.() -> Unit,
+    itemKey: (Int, T) -> String,
+    renderHeader: (@Composable () -> Unit)? = null,
+    renderListItem: @Composable (T) -> Unit,
+) {
+  val isEmptyList = remember(list) { list.isEmpty() }
+
+  LazyColumn(
+      modifier = modifier,
+      contentPadding = PaddingValues(horizontal = MaterialTheme.keylines.baseline),
+  ) {
+    if (renderHeader != null) {
+      item { renderHeader() }
+    }
+
+    stickyHeader {
+      Column(
+          modifier = Modifier.fillMaxWidth(),
+      ) {
+        Spacer(
+            modifier = Modifier.statusBarsHeight(),
+        )
+        SearchBar(
+            modifier = Modifier.fillMaxWidth(),
+            search = search,
+            currentTab = tab,
+            onSearchChanged = onSearchChanged,
+            onTabUpdated = onTabUpdated,
+            onRegenerateList = onRegenerateList,
+        )
+      }
+    }
+
+    when {
+      pageError != null -> {
+        item {
+          ErrorState(
+              modifier = Modifier.fillMaxSize(),
+              imageLoader = imageLoader,
+              error = pageError,
+              onRefresh = onRefresh,
+          )
+        }
+      }
+      isEmptyList -> {
+        item {
+          EmptyState(
+              modifier = Modifier.fillMaxWidth(),
+              imageLoader = imageLoader,
+              image = emptyStateImage,
+              label = emptyStateLabel,
+          )
+        }
+      }
+      else -> {
+        itemsIndexed(
+            items = list,
+            key = { index, item -> itemKey(index, item) },
+        ) { index, item ->
+          if (index == 0) {
+            Spacer(
+                modifier = Modifier.height(MaterialTheme.keylines.content),
+            )
+          }
+
+          renderListItem(item)
+
+          Spacer(
+              modifier = Modifier.height(MaterialTheme.keylines.content),
+          )
+        }
+      }
+    }
+
+    item {
+      Spacer(
+          modifier =
+              Modifier.navigationBarsHeight(
+                  additional =
+                      navBarBottomHeight +
+                          FabDefaults.FAB_OFFSET_DP +
+                          (MaterialTheme.keylines.content * 2),
+              ),
+      )
+    }
+  }
+}
+
+@Composable
+private fun EmptyState(
+    modifier: Modifier = Modifier,
+    imageLoader: ImageLoader,
+    @DrawableRes image: Int,
+    label: String,
+) {
+  PolinaGolubevaScreen(
+      modifier = modifier,
+      imageLoader = imageLoader,
+      image = image,
+      bottomContent = {
+        Text(
+            modifier = Modifier.padding(horizontal = MaterialTheme.keylines.content),
+            text = label,
+            style = MaterialTheme.typography.h6,
+        )
+      },
+  )
+}
+
+@Composable
+private fun ErrorState(
+    modifier: Modifier = Modifier,
+    imageLoader: ImageLoader,
+    error: Throwable,
+    onRefresh: () -> Unit,
+) {
+  ErrorScreen(
+      modifier = modifier,
+      imageLoader = imageLoader,
+      bottomContent = {
+        Text(
+            textAlign = TextAlign.Center,
+            text = error.message ?: "An unexpected error occurred",
+            style =
+                MaterialTheme.typography.body1.copy(
+                    color = MaterialTheme.colors.error,
+                ),
+        )
+
+        Text(
+            modifier = Modifier.padding(top = MaterialTheme.keylines.content),
+            textAlign = TextAlign.Center,
+            text = "Please try again later.",
+            style = MaterialTheme.typography.body2,
+        )
+
+        Button(
+            modifier = Modifier.padding(top = MaterialTheme.keylines.content),
+            onClick = onRefresh,
+        ) {
+          Text(
+              text = "Refresh",
+          )
+        }
+      },
+  )
+}
+
+@Preview
+@Composable
+private fun PreviewBaseListScreen() {
+  BaseListScreen(
+      modifier = Modifier.fillMaxSize(),
+      imageLoader = createNewTestImageLoader(),
+      onRefresh = {},
+      onSearchChanged = {},
+      onTabUpdated = {},
+      onFabClick = {},
+      onRegenerateList = {},
+      pageError = null,
+      list = emptyList(),
+      tab = EquityType.STOCK,
+      isLoading = false,
+      renderListItem = {},
+      renderHeader = null,
+      emptyStateImage = 0,
+      emptyStateLabel = "Empty State",
+      itemKey = { _, _ -> "" },
+      search = "",
+  )
+}

79edd1bb2fbc7d462da55709c1c5879ca37510fd
pyamsoft <developer.pyamsoft@gmail.com>
2022-07-21 00:22:39 -0700

    Positions at front

--- portfolio/src/main/java/com/pyamsoft/tickertape/portfolio/dig/PortfolioDigSections.kt
+++ portfolio/src/main/java/com/pyamsoft/tickertape/portfolio/dig/PortfolioDigSections.kt
@@ -1,9 +1,9 @@
 package com.pyamsoft.tickertape.portfolio.dig
 
 enum class PortfolioDigSections(val display: String) {
+  POSITIONS("Positions"),
   CHART("Chart"),
   STATISTICS("Details"),
   NEWS("News"),
-  POSITIONS("Positions"),
   SPLITS("Stock Splits"),
 }

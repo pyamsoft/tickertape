6ca94c6c993e206ec3c2ce65fab2c5c841204c4d
pyamsoft <developer.pyamsoft@gmail.com>
2023-01-21 20:44:44 -0800

    Create home entry

--- app/src/main/java/com/pyamsoft/tickertape/home/HomeEntry.kt
+++ app/src/main/java/com/pyamsoft/tickertape/home/HomeEntry.kt
@@ -16,10 +16,19 @@
 
 package com.pyamsoft.tickertape.home
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.fragment.app.FragmentActivity
 import coil.ImageLoader
 import com.pyamsoft.pydroid.ui.inject.ComposableInjector
+import com.pyamsoft.pydroid.ui.inject.rememberComposableInjector
+import com.pyamsoft.pydroid.ui.util.rememberNotNull
 import com.pyamsoft.tickertape.ObjectGraph
+import com.pyamsoft.tickertape.quote.Ticker
+import com.pyamsoft.tickertape.setting.SettingsDialog
 import javax.inject.Inject
 
 class HomeInjector @Inject internal constructor() : ComposableInjector() {
@@ -34,5 +43,95 @@ class HomeInjector @Inject internal constructor() : ComposableInjector() {
 
   override fun onInject(activity: FragmentActivity) {
     ObjectGraph.ActivityScope.retrieve(activity).plusHome().create().inject(this)
+  }
+}
+
+@Composable
+fun HomeEntry(
+    modifier: Modifier = Modifier,
+    appName: String,
+    onOpenWatchlistDig: (Ticker) -> Unit,
+) {
+  val component = rememberComposableInjector { HomeInjector() }
+  val viewModel = rememberNotNull(component.viewModel)
+  val imageLoader = rememberNotNull(component.imageLoader)
+
+  val state = viewModel.state
+  val isSettingsOpen by state.isSettingsOpen.collectAsState()
+
+  HomeScreen(
+      modifier = modifier,
+      state = state,
+      appName = appName,
+      imageLoader = imageLoader,
+      onSettingsClicked = { viewModel.handleOpenSettings() },
+      onChartClicked = { onOpenWatchlistDig(it) },
+      onRefreshWatchlist = {
+        viewModel.handleFetchWatchlist(
+            scope = this,
+            force = false,
+        )
+      },
+      onRefreshUndervaluedGrowth = {
+        viewModel.handleFetchUndervaluedGrowth(
+            scope = this,
+            force = false,
+        )
+      },
+      onRefreshTrending = {
+        viewModel.handleFetchTrending(
+            scope = this,
+            force = false,
+        )
+      },
+      onRefreshPortfolio = {
+        viewModel.handleFetchPortfolio(
+            scope = this,
+            force = false,
+        )
+      },
+      onRefreshMostShorted = {
+        viewModel.handleFetchMostShorted(
+            scope = this,
+            force = false,
+        )
+      },
+      onRefreshLosers = {
+        viewModel.handleFetchLosers(
+            scope = this,
+            force = false,
+        )
+      },
+      onRefreshIndexes = {
+        viewModel.handleFetchIndexes(
+            scope = this,
+            force = false,
+        )
+      },
+      onRefreshGrowthTech = {
+        viewModel.handleFetchGrowthTech(
+            scope = this,
+            force = false,
+        )
+      },
+      onRefreshGainers = {
+        viewModel.handleFetchGainers(
+            scope = this,
+            force = false,
+        )
+      },
+      onRefreshMostActive = {
+        viewModel.handleFetchMostActive(
+            scope = this,
+            force = false,
+        )
+      },
+  )
+
+  if (isSettingsOpen) {
+    SettingsDialog(
+        modifier = Modifier.fillMaxSize(),
+        onDismiss = { viewModel.handleCloseSettings() },
+    )
   }
 }
--- app/src/main/java/com/pyamsoft/tickertape/home/HomeFragment.kt
+++ app/src/main/java/com/pyamsoft/tickertape/home/HomeFragment.kt
@@ -16,59 +16,31 @@
 
 package com.pyamsoft.tickertape.home
 
-import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.CheckResult
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import coil.ImageLoader
 import com.pyamsoft.pydroid.core.requireNotNull
 import com.pyamsoft.pydroid.ui.navigator.FragmentNavigator
-import com.pyamsoft.pydroid.ui.navigator.Navigator
 import com.pyamsoft.pydroid.ui.theme.ThemeProvider
 import com.pyamsoft.pydroid.ui.theme.Theming
-import com.pyamsoft.pydroid.ui.util.dispose
-import com.pyamsoft.pydroid.ui.util.recompose
 import com.pyamsoft.tickertape.ObjectGraph
 import com.pyamsoft.tickertape.R
 import com.pyamsoft.tickertape.main.MainPage
+import com.pyamsoft.tickertape.main.MainViewModeler
 import com.pyamsoft.tickertape.main.TopLevelMainPage
-import com.pyamsoft.tickertape.quote.Ticker
-import com.pyamsoft.tickertape.stocks.api.StockOptionsQuote
 import com.pyamsoft.tickertape.ui.TickerTapeTheme
 import javax.inject.Inject
-import kotlinx.coroutines.CoroutineScope
-import timber.log.Timber
 
 class HomeFragment : Fragment(), FragmentNavigator.Screen<MainPage> {
 
-  @JvmField @Inject internal var navigator: Navigator<MainPage>? = null
-  @JvmField @Inject internal var viewModel: HomeViewModeler? = null
   @JvmField @Inject internal var theming: Theming? = null
-  @JvmField @Inject internal var imageLoader: ImageLoader? = null
-
-  private fun onOpenDigDialog(ticker: Ticker) {
-    val quote = ticker.quote
-    if (quote == null) {
-      Timber.w("Can't show dig dialog, missing quote: ${ticker.symbol}")
-      return
-    }
-
-    val equityType = quote.type
-    val lookupSymbol = if (quote is StockOptionsQuote) quote.underlyingSymbol else quote.symbol
-    // TODO open WatchDigEntry
-  }
-
-  private fun onOpenSettingsDialog() {
-    // TODO
-  }
+  @JvmField @Inject internal var mainViewModel: MainViewModeler? = null
 
   override fun onCreateView(
       inflater: LayoutInflater,
@@ -80,103 +52,26 @@ class HomeFragment : Fragment(), FragmentNavigator.Screen<MainPage> {
     ObjectGraph.ActivityScope.retrieve(act).plusHome().create().inject(this)
 
     val appName = act.getString(R.string.app_name)
-    val vm = viewModel.requireNotNull()
-    val loader = imageLoader.requireNotNull()
 
     val themeProvider = ThemeProvider { theming.requireNotNull().isDarkTheme(act) }
     return ComposeView(act).apply {
       id = R.id.screen_home
 
       setContent {
-        val handleOpenSettings by rememberUpdatedState { onOpenSettingsDialog() }
-
-        val handleChartClicked by rememberUpdatedState { ticker: Ticker -> onOpenDigDialog(ticker) }
-
-        val handleRefreshWatchlist by
-            rememberUpdatedState<CoroutineScope.() -> Unit> { vm.handleFetchWatchlist(this, false) }
-
-        val handleRefreshUndervaluedGrowth by
-            rememberUpdatedState<CoroutineScope.() -> Unit> {
-              vm.handleFetchUndervaluedGrowth(this, false)
-            }
-
-        val handleRefreshTrending by
-            rememberUpdatedState<CoroutineScope.() -> Unit> { vm.handleFetchTrending(this, false) }
-
-        val handleRefreshPortfolio by
-            rememberUpdatedState<CoroutineScope.() -> Unit> { vm.handleFetchPortfolio(this, false) }
-
-        val handleRefreshMostShorted by
-            rememberUpdatedState<CoroutineScope.() -> Unit> {
-              vm.handleFetchMostShorted(this, false)
-            }
-
-        val handleRefreshLosers by
-            rememberUpdatedState<CoroutineScope.() -> Unit> { vm.handleFetchLosers(this, false) }
-
-        val handleRefreshGainers by
-            rememberUpdatedState<CoroutineScope.() -> Unit> { vm.handleFetchGainers(this, false) }
-
-        val handleRefreshIndexes by
-            rememberUpdatedState<CoroutineScope.() -> Unit> { vm.handleFetchIndexes(this, false) }
-
-        val handleRefreshGrowthTech by
-            rememberUpdatedState<CoroutineScope.() -> Unit> {
-              vm.handleFetchGrowthTech(this, false)
-            }
-
-        val handleRefreshMostActive by
-            rememberUpdatedState<CoroutineScope.() -> Unit> {
-              vm.handleFetchMostActive(this, false)
-            }
-
         act.TickerTapeTheme(themeProvider) {
-          HomeScreen(
+          HomeEntry(
               modifier = Modifier.fillMaxSize(),
-              state = vm.state,
               appName = appName,
-              imageLoader = loader,
-              onSettingsClicked = handleOpenSettings,
-              onChartClicked = handleChartClicked,
-              onRefreshWatchlist = handleRefreshWatchlist,
-              onRefreshUndervaluedGrowth = handleRefreshUndervaluedGrowth,
-              onRefreshTrending = handleRefreshTrending,
-              onRefreshPortfolio = handleRefreshPortfolio,
-              onRefreshMostShorted = handleRefreshMostShorted,
-              onRefreshLosers = handleRefreshLosers,
-              onRefreshIndexes = handleRefreshIndexes,
-              onRefreshGrowthTech = handleRefreshGrowthTech,
-              onRefreshGainers = handleRefreshGainers,
-              onRefreshMostActive = handleRefreshMostActive,
-          )
+              onOpenWatchlistDig = { mainViewModel.requireNotNull().handleOpenDig(it) })
         }
       }
     }
   }
 
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-    viewModel.requireNotNull().restoreState(savedInstanceState)
-  }
-
-  override fun onSaveInstanceState(outState: Bundle) {
-    super.onSaveInstanceState(outState)
-    viewModel?.saveState(outState)
-  }
-
-  override fun onConfigurationChanged(newConfig: Configuration) {
-    super.onConfigurationChanged(newConfig)
-    recompose()
-  }
-
   override fun onDestroyView() {
     super.onDestroyView()
-    dispose()
 
     theming = null
-    viewModel = null
-    imageLoader = null
-    navigator = null
   }
 
   override fun getScreenId(): MainPage {
